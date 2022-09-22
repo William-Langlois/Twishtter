5c9c737855c9d04ee39295722c24612ce6b19c2c
William-Langlois <langlois.william.pro@gmail.com>
2022-09-22 23:45:59 +0200

    commit

--- type.d.ts
+++ type.d.ts
@@ -1,12 +1,11 @@
 import type { DefaultUser } from 'next-auth';
 
-declare module 'module'  {
-    interface userInfo {
-        phone:String;
-        firstname:String;
-        lastname:String;
-    }
+interface userInfo {
+    phone:String;
+    firstname:String;
+    lastname:String;
 }
+
 
 declare module 'next-auth' {
     interface Session {
