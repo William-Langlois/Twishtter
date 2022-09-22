6fb83db2fc5ad736d3e1cd179b26a578142c202a
William-Langlois <langlois.william.pro@gmail.com>
2022-09-22 23:44:48 +0200

    commit

--- pages/api/user/[id]/updateInfos/[infoId].ts
+++ pages/api/user/[id]/updateInfos/[infoId].ts
@@ -21,11 +21,9 @@ export default async function handle(req, res) {
         const result = await prisma.userInfo.update({
             where:{id:String(userinfoId)},
             data: { 
-                /*
                 phone: infos.phone,
                 firstname:infos.firstname,
                 lastname:infos.lastname,
-                */
                 users: {connect:{id:userId}}
             },
         });
--- type.d.ts
+++ type.d.ts
@@ -1,5 +1,12 @@
 import type { DefaultUser } from 'next-auth';
 
+declare module 'module'  {
+    interface userInfo {
+        phone:String;
+        firstname:String;
+        lastname:String;
+    }
+}
 
 declare module 'next-auth' {
     interface Session {
